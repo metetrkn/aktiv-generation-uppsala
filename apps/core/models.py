ea4d57fe99a5f328bfd20718f402e3bfd7635de4
metetrkn <metetrkn52@gmail.com>
2025-07-17 23:27:45 +0200

    Add deletefunction for system files for activities     - Add delete function in core/models.py

--- apps/core/models.py
+++ apps/core/models.py
@@ -11,6 +11,8 @@ Models included:
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+import os
+from django.conf import settings
 
 
 class Organization(models.Model):
@@ -96,3 +98,10 @@ class ActivityImage(models.Model):
 
     def __str__(self):
         return f"Image for {self.activity.title}"
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            image_path = self.image.path
+            if os.path.exists(image_path):
+                os.remove(image_path)
+        super().delete(*args, **kwargs)
