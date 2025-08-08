c684a65b630cb9130a30168d9761d6d33833990d
metetrkn <metetrkn52@gmail.com>
2025-08-08 16:21:57 +0200

    ikinci commit, neler degistigi yazilmali

--- configuration/settings/development.py
+++ configuration/settings/development.py
@@ -1,11 +1,38 @@
 from .base import *
 
 DEBUG = True
+
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-DATABASES['default']['NAME'] = os.environ.get('DB_NAME_DEV', 'postgres')
-DATABASES['default']['USER'] = os.environ.get('DB_USER_DEV', 'postgres')
-DATABASES['default']['PASSWORD'] = os.environ.get('DB_PASSWORD_DEV', 'postgres')
-DATABASES['default']['HOST'] = os.environ.get('DB_HOST_DEV', 'localhost')
-DATABASES['default']['PORT'] = os.environ.get('DB_PORT_DEV', '5432')
-DATABASES['default']['OPTIONS'] = {'sslmode': 'disable'}
+INTERNAL_IPS = ["127.0.0.1"]
+
+# Debug toolbar
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+# Database
+DATABASES['default'].update({
+    'NAME': os.environ.get('DB_NAME_DEV', 'postgres'),
+    'USER': os.environ.get('DB_USER_DEV', 'postgres'),
+    'PASSWORD': os.environ.get('DB_PASSWORD_DEV', 'postgres'),
+    'HOST': os.environ.get('DB_HOST_DEV', 'localhost'),
+    'PORT': os.environ.get('DB_PORT_DEV', '5432'),
+    'OPTIONS': {'sslmode': 'disable'}
+})
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Disable security features for development
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
+
+# Debug logging
+LOGGING['root']['level'] = 'DEBUG'
+LOGGING['loggers'] = {
+    'django': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+        'propagate': False,
+    },
+}
--- configuration/settings/production.py
+++ configuration/settings/production.py
@@ -1,11 +1,44 @@
 from .base import *
+import os
 
 DEBUG = False
+
+# Security settings
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",")
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-DATABASES['default']['NAME'] = os.environ.get('DB_NAME_PROD', 'aktiv_generation')
-DATABASES['default']['USER'] = os.environ.get('DB_USER_PROD', 'postgres')
-DATABASES['default']['PASSWORD'] = os.environ.get('DB_PASSWORD_PROD', 'bjk123394')
-DATABASES['default']['HOST'] = os.environ.get('DB_HOST_PROD', 'localhost')
-DATABASES['default']['PORT'] = os.environ.get('DB_PORT_PROD', '5432')
-DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_REFERRER_POLICY = 'same-origin'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Database
+DATABASES['default'].update({
+    'NAME': os.environ.get('DB_NAME_PROD'),
+    'USER': os.environ.get('DB_USER_PROD'),
+    'PASSWORD': os.environ.get('DB_PASSWORD_PROD'),
+    'HOST': os.environ.get('DB_HOST_PROD'),
+    'PORT': os.environ.get('DB_PORT_PROD', '5432'),
+    'OPTIONS': {'sslmode': 'require'}
+})
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Additional production-only apps
+INSTALLED_APPS.extend([
+    # Any production-specific apps
+])
+
+# Production logging
+LOGGING['handlers']['file'] = {
+    'level': 'INFO',
+    'class': 'logging.FileHandler',
+    'filename': '/var/log/django/app.log',
+}
+LOGGING['root']['handlers'].append('file')
