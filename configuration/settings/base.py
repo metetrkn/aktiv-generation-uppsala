b298a228630abca0e8c524a044da99e6a38b8b35
metetrkn <metetrkn52@gmail.com>
2025-08-10 15:02:13 +0200

    Configurate e-mail settings based on prod-dev     - settings/base.py: Remove SMTP e-mail settings     - settings/production.py Add SMTP e-mail settings

--- configuration/settings/base.py
+++ configuration/settings/base.py
@@ -96,15 +96,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email (basic config only)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
 # Logging (basic config)
 LOGGING = {
     'version': 1,
--- configuration/settings/production.py
+++ configuration/settings/production.py
@@ -27,8 +27,13 @@ DATABASES['default'].update({
     'OPTIONS': {'sslmode': 'require'}
 })
 
-# Email
+# Email (SMTP for production)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Additional production-only apps
 INSTALLED_APPS.extend([
